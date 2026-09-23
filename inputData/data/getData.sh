426924f00227365a58e51418c8fee77b3b06e773
Hanbing59 <xingzhe.phb@gmail.com>
2026-09-23 10:59:20 +0200

    Delete `/inputData/`

--- inputData/data/getData.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../../scripts/demoData/getFiles.sh data.list https://peanpod.s3.ap-southeast-2.amazonaws.com/aux/data ./
--- inputData/products/getProducts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-../../scripts/demoData/getFiles.sh products.list https://peanpod.s3.ap-southeast-2.amazonaws.com/aux/products ./
